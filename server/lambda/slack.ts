048a5f34bba094f591b129e343ddbc4856636dd4
Takashi Suzuki <42316228+k-tokitoh@users.noreply.github.com>
2025-01-23 16:44:10 +0900

    スレッド内の会話履歴を与える

--- server/lambda/slack.ts
+++ server/lambda/slack.ts
@@ -3,6 +3,7 @@ import { handle, LambdaContext, LambdaEvent } from "hono/aws-lambda";
 import { WebClient } from "@slack/web-api";
 import type { AppMentionEvent } from "@slack/types";
 import { ChatOpenAI } from "@langchain/openai";
+import { SystemMessage, HumanMessage } from "@langchain/core/messages";
 
 const web = new WebClient(process.env.SLACK_TOKEN);
 
@@ -18,8 +19,6 @@ app.post("/webhook", async (c) => {
   const reqBody = await c.req.json();
   const event: AppMentionEvent = reqBody.event;
 
-  // console.log({ req }, { depth: null });
-
   if (reqBody.type === "url_verification") {
     return c.text(reqBody.challenge);
   }
@@ -27,17 +26,28 @@ app.post("/webhook", async (c) => {
   // lambdaで直ちにレスポンスしても、たぶんcold startのせいで3秒ルールに引っかかってリトライされてしまうので、雑に回避
   if (c.req.header("X-Slack-Retry-Num") !== undefined) return;
 
+  const replies = await web.conversations.replies({
+    channel: event.channel,
+    // スレッド全体を取得するには以下を指定する必要あり
+    // - スレッド内2つめ以降のメッセージの場合
+    //   - event.thread_ts を利用する
+    // - スレッドの最初のメッセージの場合
+    //   - event.ts を利用する（thread_tsはundefined）
+    ts: event.thread_ts ?? event.ts,
+    limit: 10,
+  });
+
   const model = new ChatOpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
     modelName: "gpt-3.5-turbo",
   });
 
-  // const messages = [
-  //   new SystemMessage("文章を日本語から英語に翻訳してください。"),
-  //   new HumanMessage("こんにちは!"),
-  // ];
+  const messages = [
+    // new SystemMessage(""),
+    ...(replies.messages ?? []).map((m) => new HumanMessage(m.text ?? "")),
+  ];
 
-  const res = await model.invoke(event.text);
+  const res = await model.invoke(messages);
 
   await web.chat.postMessage({
     channel: event.channel,
